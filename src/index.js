4d6bb1c9c3757cdf3a03552f5c1a077a26f4b412
Sam Tricamo <samualtricamo@gmail.com>
2019-10-15 16:57:23 -0500

    clicky js added

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import react from 'react';
+import reactDOM from 'react-dom';
+import clickyGame from './comonents/clickygame';
+import './index.css';
+
+reactDOM.render(<clickyGame /> document.getElementById('root'));
